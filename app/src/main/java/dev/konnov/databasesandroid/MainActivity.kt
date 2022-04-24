687c0608d21de3a66846e814842c6a9042625774
ilya k <konnovdev@gmail.com>
2022-04-24 13:43:24 +0800

    Removed all the theming, because this app will be using the standard material theme

--- app/src/main/java/dev/konnov/databasesandroid/MainActivity.kt
+++ app/src/main/java/dev/konnov/databasesandroid/MainActivity.kt
@@ -3,41 +3,48 @@ package dev.konnov.databasesandroid
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material.Button
 import androidx.compose.material.MaterialTheme
 import androidx.compose.material.Surface
 import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
 import dev.konnov.databasesandroid.ui.theme.DatabasesAndroidTheme
 
 class MainActivity : ComponentActivity() {
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContent {
-            DatabasesAndroidTheme {
-                // A surface container using the 'background' color from the theme
-                Surface(
-                    modifier = Modifier.fillMaxSize(),
-                    color = MaterialTheme.colors.background
-                ) {
-                    Greeting("Android")
-                }
-            }
+            MainScreen()
         }
     }
 }
 
 @Composable
-fun Greeting(name: String) {
-    Text(text = "Hello $name!")
+@Preview(showBackground = true, widthDp = 320, heightDp = 700)
+private fun PreviewMainScreen() {
+    MainScreen()
 }
 
-@Preview(showBackground = true)
 @Composable
-fun DefaultPreview() {
+private fun MainScreen() {
     DatabasesAndroidTheme {
-        Greeting("Android")
+        Surface(
+            modifier = Modifier.fillMaxSize(),
+            color = MaterialTheme.colors.background
+        ) {
+            Column(Modifier.fillMaxSize().padding(32.dp)) {
+                Text(text = "Choose a db to test")
+                Button(onClick = { /*TODO open sql screen*/ }) {
+                    Text(text = "SqliteHelper")
+                }
+            }
+        }
     }
 }
