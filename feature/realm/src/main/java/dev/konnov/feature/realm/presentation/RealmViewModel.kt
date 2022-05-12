919c55a6e333f99cbac3896fc8f2595ed94da54c
ilya k <konnovdev@gmail.com>
2022-05-12 23:16:07 +0800

    Added weather data repository for working with realm

--- feature/realm/src/main/java/dev/konnov/feature/realm/presentation/RealmViewModel.kt
+++ feature/realm/src/main/java/dev/konnov/feature/realm/presentation/RealmViewModel.kt
@@ -6,12 +6,12 @@ import dagger.hilt.android.lifecycle.HiltViewModel
 import dev.konnov.common.dataset.newsreports.NewsReport
 import dev.konnov.common.dataset.newsreports.NewsReportDataGenerator
 import dev.konnov.common.dataset.newsreports.Title
+import dev.konnov.common.dataset.weatherlogs.Temperature
+import dev.konnov.common.dataset.weatherlogs.WeatherLog
 import dev.konnov.common.dataset.weatherlogs.WeatherLogDataGenerator
-import dev.konnov.common.dbtestingtools.SIZE_100k
-import dev.konnov.common.dbtestingtools.SIZE_10k
-import dev.konnov.common.dbtestingtools.TestResult
-import dev.konnov.common.dbtestingtools.group
+import dev.konnov.common.dbtestingtools.*
 import dev.konnov.feature.realm.data.repository.NewsReportRepository
+import dev.konnov.feature.realm.data.repository.WeatherDataRepository
 import kotlinx.coroutines.flow.MutableStateFlow
 import kotlinx.coroutines.flow.StateFlow
 import kotlinx.coroutines.launch
@@ -21,7 +21,8 @@ import javax.inject.Inject
 
 @HiltViewModel
 class RealmViewModel @Inject constructor(
-    private val newsReportRepository: NewsReportRepository
+    private val newsReportRepository: NewsReportRepository,
+    private val weatherDataRepository: WeatherDataRepository,
 ) : ViewModel() {
 
     private val _state = MutableStateFlow<RealmViewState>(InProgress)
@@ -32,13 +33,33 @@ class RealmViewModel @Inject constructor(
 // TODO create an entity class here that will be returned on UI and displayed
             val results = mutableListOf<TestResult>()
             for (i in 0..TEST_ITERATIONS) {
-//                    results += testOnWeather(WeatherLogDataGenerator.getEntities(SIZE_10k)) // TODO add weather tests
+                results += testOnWeather(WeatherLogDataGenerator.getEntities(SIZE_10k))
                 results += testOnNews(NewsReportDataGenerator.getEntities(SIZE_10k))
-//                    results += testOnWeather(WeatherLogDataGenerator.getEntities(SIZE_100k))
+                results += testOnWeather(WeatherLogDataGenerator.getEntities(SIZE_100k))
                 results += testOnNews(NewsReportDataGenerator.getEntities(SIZE_100k))
             }
             _state.value = Content(results.group())
         }
+    }
+
+    private suspend fun testOnWeather(entries: List<WeatherLog>): List<TestResult> {
+        val oldTemperature = Temperature(13.0)
+        val newTemperature = Temperature(14.0)
+        val weatherLogToInsert = WeatherLog(newTemperature, 3123.1, 33.0)
+
+        val insertResult = weatherDataRepository.insert(entries)
+        val loadEverythingResult = weatherDataRepository.loadEverything()
+        val updateResult = weatherDataRepository.update(oldTemperature, weatherLogToInsert)
+        val loadByParameterResult = weatherDataRepository.loadByParameter(newTemperature)
+        val deleteResult = weatherDataRepository.delete(newTemperature)
+
+        return listOf(
+            insertResult,
+            loadEverythingResult,
+            updateResult,
+            loadByParameterResult,
+            deleteResult
+        )
     }
 
     private suspend fun testOnNews(entries: List<NewsReport>): List<TestResult> {
@@ -62,6 +83,6 @@ class RealmViewModel @Inject constructor(
     }
 
     private companion object {
-        const val TEST_ITERATIONS = 5
+        const val TEST_ITERATIONS = 1
     }
 }
