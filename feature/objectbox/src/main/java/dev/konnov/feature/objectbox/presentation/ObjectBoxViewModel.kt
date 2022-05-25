b91ad1d09e989eac5a55ee42dd88b63b9447c037
ilya k <konnovdev@gmail.com>
2022-05-26 02:02:40 +0800

    Moved ObjectBox to IO thread

--- feature/objectbox/src/main/java/dev/konnov/feature/objectbox/presentation/ObjectBoxViewModel.kt
+++ feature/objectbox/src/main/java/dev/konnov/feature/objectbox/presentation/ObjectBoxViewModel.kt
@@ -1,9 +1,14 @@
 package dev.konnov.feature.objectbox.presentation
 
+import androidx.lifecycle.viewModelScope
 import dagger.hilt.android.lifecycle.HiltViewModel
 import dev.konnov.common.dbtestingtools.DbInfo
 import dev.konnov.common.dbtestingtools.domain.usecase.TestSpeedUseCase
 import dev.konnov.common.mvvm.TestDbViewModel
+import dev.konnov.common.mvvm.TestDbViewState
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.launch
+import kotlinx.coroutines.withContext
 import javax.inject.Inject
 import javax.inject.Named
 
@@ -11,11 +16,17 @@ import javax.inject.Named
 @HiltViewModel
 class ObjectBoxViewModel @Inject constructor(
     @Named("Objectbox_usecase")
-    testSpeedUseCase: TestSpeedUseCase
+    private val testSpeedUseCase: TestSpeedUseCase
 ): TestDbViewModel(testSpeedUseCase) {
 
     override val dbInfo = DbInfo("Object Box")
 
-    // todo перевести работу на io тред
-    override val testIterations = 1
+    override fun testDbSpeed() {
+        viewModelScope.launch(Dispatchers.IO) {
+            val testResults = testSpeedUseCase(testIterations, testSizes)
+            withContext(Dispatchers.Main) {
+                _state.value = TestDbViewState.Content(dbInfo, testResults)
+            }
+        }
+    }
 }
